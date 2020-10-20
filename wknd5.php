b91e668b2432c037079977fd4d8db6513171d0a6
palstheintrovert <palash.idt@gmail.com>
2020-10-20 20:56:58 +0600

    Create wknd5.php

---
+++ wknd5.php
@@ -0,0 +1,24 @@
+
+
+document.write('<style type="text/css">');
+document.write('td.mojo_copyright { text-align: center; background-color: #000000; font-size: 7pt; font-family: arial; }' );
+document.write('a.mojocopyra:link, a.mojocopyra:visited, a.mojocopyra:active { color: #ffffff; font-size: 7pt; font-family: arial; }');
+document.write('a.mojocopyra:hover { color: #ffffff; font-size: 7pt; font-family: arial; text-decoration: underline; background: none }');
+document.write('td.mojo_header { text-align: center; background-color: #ff8c1a; font-size: 9pt; font-family: verdana; font-weight: bold; color: #ffffff }');
+document.write('td.mojo_row { background-color: #000; font-size: 10pt; font-family: verdana; color: #fff }');
+document.write('</style>');
+document.write('<table border=0 cellspacing=1 cellpadding=3>');
+document.write('<tr><td align=center colspan=2 class=mojo_header><nobr>Weekend Box Office</nobr>, <nobr>Oct 16&#150;18</nobr></td></tr>');
+
+    document.write('<tr><td class=mojo_row>1. Honest Thief</td><td align=right class=mojo_row nowrap>$4.1 M</td></tr>');
+
+    document.write('<tr><td class=mojo_row>2. The War with Grandpa</td><td align=right class=mojo_row nowrap>$2.5 M</td></tr>');
+
+    document.write('<tr><td class=mojo_row>3. Tenet</td><td align=right class=mojo_row nowrap>$1.6 M</td></tr>');
+
+    document.write('<tr><td class=mojo_row>4. The Nightmare Before Christmas 2020 Re-release</td><td align=right class=mojo_row nowrap>$1.3 M</td></tr>');
+
+    document.write('<tr><td class=mojo_row>5. Hocus Pocus 2020 Re-release</td><td align=right class=mojo_row nowrap>$756.0 K</td></tr>');
+
+document.write('<tr><td colspan=2 class=mojo_copyright><a class=mojocopyra href=http://www.boxofficemojo.com/><nobr>Source: Box Office Mojo</nobr> <nobr>Click for more info.</nobr></a></tr>');
+document.write('</table>');
